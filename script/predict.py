3c31e808ab29d25b4ca5563a317e015a421333f9
jvscursulim <josevictor.s.scursulim@gmail.com>
2022-10-27 05:12:23 -0300

    update on predict.py

--- script/predict.py
+++ script/predict.py
@@ -1,1 +1,33 @@
+# imports
+import pickle
+from flask import Flask, jsonify, request
 
+# defining a flask app
+app = Flask(__name__)
+
+#
+@app.route("/predict", methods=["POST"])
+def predict():
+    
+    # loading the model using pickle
+    with open(file="../model/model.bin", mode="rb") as model_file:
+        
+        model = pickle.load(model_file)
+    
+    # loading the DictVectorizer using pickle    
+    with open(file="../model/dv.bin", mode="rb") as dv_file:
+        
+        dv = pickle.load(dv_file)
+        
+    client = request.get_json()
+    
+    X = dv.transform([client])
+    y_pred = model.predict_proba(X)[0, 1]
+    
+    result = {"heart_failure_prediction": y_pred}
+    
+    return jsonify(result)
+
+if __name__ == "__main__":
+    
+    app.run(debug=True, host="0.0.0.0", port=4242)
